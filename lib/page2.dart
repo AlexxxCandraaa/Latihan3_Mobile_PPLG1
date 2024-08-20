f4dc288d985fcb75a8b6354cf9c098bbf5253e14
AlexxxCandraaa <Alexmarhas59@gmail.com>
2024-08-20 11:45:13 +0700

    Commit Pertama

---
+++ lib/page2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Page2 extends StatelessWidget {
+  const Page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Text('Ini Halaman 2'),
+    ));
+  }
+}
